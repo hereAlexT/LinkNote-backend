288329108d4b1a671016057f1ab4b01f47623de2
ShaomingT <alex.teng.net@gmail.com>
2023-11-22 12:53:19 +1100

    fixing case sensitive issue

---
+++ frontend/linknote/src/pages/Signup.tsx
@@ -0,0 +1,103 @@
+import { IonContent, IonHeader, IonInput, IonPage, IonText, IonTitle, IonToolbar, IonButton, IonGrid, IonCol, IonRow, IonButtons,IonMenuButton } from '@ionic/react';
+import { useState } from 'react';
+
+
+const Signup: React.FC = () => {
+    const [isTouched, setIsTouched] = useState(false);
+    const [isValid, setIsValid] = useState<boolean>();
+
+    const validateEmail = (email: string) => {
+        return email.match(
+            /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+        );
+    };
+
+    const validate = (ev: Event) => {
+        const value = (ev.target as HTMLInputElement).value;
+
+        setIsValid(undefined);
+
+        if (value === '') return;
+
+        validateEmail(value) !== null ? setIsValid(true) : setIsValid(false);
+    };
+
+    const markTouched = () => {
+        setIsTouched(true);
+    };
+
+
+
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    {/* <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons> */}
+                    <IonTitle>Sign up</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Signup</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonGrid className='ion-padding'>
+                    <IonRow>
+                        <IonInput
+                            className={`${isValid && 'ion-valid'} ${isValid === false && 'ion-invalid'} ${isTouched && 'ion-touched'}`}
+                            type="email"
+                            fill="solid"
+                            label="Email"
+                            labelPlacement="floating"
+                            helperText="Enter a valid email"
+                            errorText="Invalid email"
+                            onIonInput={(event) => validate(event)}
+                            onIonBlur={() => markTouched()} />
+                    </IonRow>
+                    <IonRow>
+                        <IonInput
+                            label="password"
+                            helperText="Type your password"
+                            labelPlacement="floating"
+                            counter={true}
+                            maxlength={32}
+                            minlength={8}></IonInput>
+                    </IonRow>
+                    <IonRow>
+                        <IonInput
+                            label="Display Name"
+                            helperText="Enter your display name"
+                            labelPlacement="floating"
+                            counter={true}
+                            maxlength={32}
+                            minlength={8}></IonInput>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol className="ion-padding-top">
+                            <IonButton expand="block" href="/timeline">Signup</IonButton>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            <IonText> - This is a pre-release version . <br /> - Data may not persistent. <br /> - Dont input sensitive data.</IonText>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+
+
+
+
+
+
+
+
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default Signup;
