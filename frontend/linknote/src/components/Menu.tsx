4076d8599d28da53c4c2ba7bc963747be2da9dbc
Alex T <79433869+hereAlexT@users.noreply.github.com>
2023-11-22 12:11:11 +1100

    Frontend deployment test (#26)
    * init the frontend
    
    * basic frontend
    
    * apply iongrid to signup
    
    * build basic ui
    
    * add search bar on timeline
    
    * set up a Component Lab to check and edit component at the same page
    
    * CardEditor layout
    
    * format some codes
    
    * added card editor to timeline
    
    * format
    
    * audit version
    
    * rename github action to e2e_deploy
    
    * remove unnecessary env settings
    
    * fix environment variable error
    
    * change workflow name
    
    * create prod workflow
    
    * modify the docker container version
    
    * remove the dependency
    
    * submit button to the right most
    
    * adjust layout
    
    * integrated with tailwindcss
    
    * fix editor and note card width not same
    
    * add note function
    
    * delete function
    
    * remove console.log
    
    * can update notes now
    
    * when editor is opened, the old note data will load
    
    * added IonMenu
    
    * delete unused component
    
    * refactor
    
    * render tab with pages
    
    * path "/" route to signup
    
    * Menu layout setup
    
    * change the max width from max-w-2xl to max-w-3xl

---
+++ frontend/linknote/src/components/Menu.tsx
@@ -0,0 +1,87 @@
+import {
+    IonContent,
+    IonList,
+    IonListHeader,
+    IonMenu,
+    IonMenuToggle,
+    IonItem,
+    IonLabel,
+    IonIcon
+} from "@ionic/react"
+
+import {
+    personOutline as personIcon,
+    flaskOutline as flaskIcon,
+    addOutline as addIcon,
+    logInOutline as logInIcon,
+    logOutOutline as logOutIcon,
+} from 'ionicons/icons';
+
+
+const routes = {
+    appPages: [
+        { title: 'Login', path: '/tabs/login', icon: logInIcon },
+        { title: 'Signup', path: '/tabs/signup', icon:  addIcon },
+        { title: 'Timeline', path: '/tabs/timeline', icon: 'time' },
+        { title: 'ComponentLab', path: '/tabs/comlab', icon: flaskIcon },
+    ],
+    loggedInPages: [
+        { title: 'Logout', path: '/tabs/logout', icon: 'log-out' },
+    ],
+    loggedOutPages: [
+        { title: 'Login', path: '/tabs/login', icon: 'log-in' },
+        { title: 'Signup', path: '/tabs/signup', icon: 'person-add' },
+    ]
+}
+
+interface Pages {
+    title: string;
+    path: string;
+    icon: string;
+    routerDirection?: string;
+}
+
+interface MenuProps {
+}
+
+const Menu: React.FC<MenuProps> = ({ }) => {
+
+    function renderlistItems(list: Pages[]) {
+        // Ensure only pages with a path are rendered
+        return list
+            .filter((route) => !!route.path)
+            .map((p) => (
+                <IonMenuToggle key={p.title} autoHide={false}>
+                    <IonItem routerLink={p.path} routerDirection="none">
+                        <IonIcon slot="start" icon={p.icon} />
+                        <IonLabel>{p.title}</IonLabel>
+                    </IonItem>
+                </IonMenuToggle>
+            ))
+
+    }
+    return (
+        <IonMenu contentId="main">
+            <IonContent>
+                <IonList>
+                    <IonListHeader>Menu</IonListHeader>
+                    {renderlistItems(routes.appPages)}
+                </IonList>
+                <IonList>
+                    <IonListHeader>Account</IonListHeader>
+                    {renderlistItems(routes.loggedInPages)}
+                </IonList>
+                <IonList>
+                    <IonListHeader>Account</IonListHeader>
+                    {renderlistItems(routes.loggedOutPages)}
+                </IonList>
+            </IonContent>
+
+
+        </IonMenu>
+
+    )
+}
+
+
+export default Menu;
